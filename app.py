0811cd7c2f0e33435acee1d50eedfc879ad55efd
RahmanAllathif <130886124+rahmanallathif2@users.noreply.github.com>
2025-01-12 16:45:19 +0700

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import psycopg2
 import pandas as pd
-import streamlit as st
-from sqlalchemy import create_engine
+import sqlite3
 from io import BytesIO
+import streamlit as st
 
 # Fungsi untuk mengimpor file Excel atau CSV
 def upload_file(uploaded_file):
@@ -15,30 +14,15 @@ def upload_file(uploaded_file):
 
 # Fungsi untuk memproses data menggunakan query SQL
 def process_data(dataregis_df, masterkel_df):
-    conn = psycopg2.connect(
-        dbname="postgres",  # Nama database PostgreSQL Anda
-        user="postgres",    # Nama user PostgreSQL Anda
-        password="Admin",   # Password PostgreSQL Anda
-        host="203.0.113.1",   # Host database Anda
-        port="5432"         # Port database Anda
-    )
+    # Membuat koneksi SQLite memory (database in-memory)
+    conn = sqlite3.connect(":memory:")
     cursor = conn.cursor()
 
-    # Membersihkan tabel sebelum menyisipkan data baru
-    cursor.execute("DELETE FROM dataregis;")
-    cursor.execute("DELETE FROM masterkel;")
+    # Menyimpan data ke SQLite
+    dataregis_df.to_sql('dataregis', conn, index=False, if_exists='replace')
+    masterkel_df.to_sql('masterkel', conn, index=False, if_exists='replace')
 
-    # Menyisipkan data ke dalam PostgreSQL (manual insert)
-    for index, row in dataregis_df.iterrows():
-        cursor.execute("INSERT INTO dataregis (no_polisi, full_address, kd_camat, kecamatan, nm_merek_kb, nm_model_kb, kd_jenis_kb, jenis_kendaraan, th_buatan, no_chasis, no_mesin, warna_kb, tg_pros_bayar) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
-                       (row['no_polisi'], row['full_address'], row['kd_camat'], row['kecamatan'], row['nm_merek_kb'], row['nm_model_kb'], row['kd_jenis_kb'], row['jenis_kendaraan'], row['th_buatan'], row['no_chasis'], row['no_mesin'], row['warna_kb'], row['tg_pros_bayar']))
-
-    for index, row in masterkel_df.iterrows():
-        cursor.execute("INSERT INTO masterkel (kelurahan, kecamatan, kelurahan_master, kecamatan_master) VALUES (%s, %s, %s, %s)", 
-                       (row['kelurahan'], row['kecamatan'], row['kelurahan_master'], row['kecamatan_master']))
-
-    conn.commit()
-
+    # Query SQL untuk memproses data
     query = """
     WITH MatchedData AS (
         SELECT 
@@ -62,12 +46,14 @@ def process_data(dataregis_df, masterkel_df):
             ROW_NUMBER() OVER (PARTITION BY dr.no_polisi ORDER BY dr.no_polisi) AS rn
         FROM dataregis dr
         LEFT JOIN masterkel mk 
-          ON dr.full_address LIKE CONCAT('%', mk.kelurahan, '%')
+          ON dr.full_address LIKE '%' || mk.kelurahan || '%'
     )
     SELECT *
     FROM MatchedData
     WHERE rn = 1;
     """
+    
+    # Eksekusi query dan ambil hasilnya
     result_df = pd.read_sql(query, conn)
     conn.close()
     return result_df
